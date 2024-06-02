0b111e9501182cde4b4d71502850e64ad76dd1b2
ramil mukhtarov <ramilasus@gmail.com>
2024-06-02 17:53:20 +0300

    13

--- travleAgency/urls.py
+++ travleAgency/urls.py
@@ -24,4 +24,7 @@ urlpatterns = [
     path('hotels/',include('Hotels.urls')),
     path('flights/',include('Airline.urls'))
     
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
